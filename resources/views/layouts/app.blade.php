175eed734bac04209ffd3be018edfa88d0bd17aa
Andi Gigatera Halil M <54834402+agthm3@users.noreply.github.com>
2023-07-21 20:07:49 +0800

    changing asset

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,16 +18,16 @@
     rel="stylesheet" />
 <!-- Icons CSS -->
 {{-- @include('includes.resources.css') --}}
-{{-- <link rel="stylesheet" href="{{ asset('css/feather.css') }}">
-<link rel="stylesheet" href="{{ asset('css/select2.css') }}"> --}}
-{{-- <link rel="stylesheet" href="{{ asset('css/dropzone.css') }}">
-<link rel="stylesheet" href="{{ asset('css/uppy.min.css') }}"> --}}
-{{-- <link rel="stylesheet" href="{{ asset('css/jquery.steps.css') }}">
-<link rel="stylesheet" href="{{ asset('css/jquery.timepicker.css') }}"> --}}
-{{-- <link rel="stylesheet" href="{{ asset('css/quill.snow.css') }}">> --}}
+<link rel="stylesheet" href="{{ asset('css/feather.css') }}">
+<link rel="stylesheet" href="{{ asset('css/select2.css') }}">
+<link rel="stylesheet" href="{{ asset('css/dropzone.css') }}">
+<link rel="stylesheet" href="{{ asset('css/uppy.min.css') }}">
+<link rel="stylesheet" href="{{ asset('css/jquery.steps.css') }}">
+<link rel="stylesheet" href="{{ asset('css/jquery.timepicker.css') }}">
+<link rel="stylesheet" href="{{ asset('css/quill.snow.css') }}">>
 
 <!-- Date Range Picker CSS -->
-<link rel="stylesheet" href="css/daterangepicker.css" />
+<link rel="stylesheet" href="asset('css/daterangepicker.css')" />
 <!-- App CSS -->
 <link rel="stylesheet" href="{{ asset('css/app-light.css') }}" id="lightTheme" />
 <link rel="stylesheet" href="{{ asset('css/app-dark.css') }}" id="darkTheme" disabled />
