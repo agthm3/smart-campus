4dee0821211eb21c05b9c14fee429e5615aedc18
Andi Gigatera Halil M <54834402+agthm3@users.noreply.github.com>
2023-07-21 20:02:22 +0800

    tring to comment bootstrap

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,13 +18,13 @@
     rel="stylesheet" />
 <!-- Icons CSS -->
 {{-- @include('includes.resources.css') --}}
-<link rel="stylesheet" href="{{ asset('css/feather.css') }}">
-<link rel="stylesheet" href="{{ asset('css/select2.css') }}">
-<link rel="stylesheet" href="{{ asset('css/dropzone.css') }}">
-<link rel="stylesheet" href="{{ asset('css/uppy.min.css') }}">
-<link rel="stylesheet" href="{{ asset('css/jquery.steps.css') }}">
-<link rel="stylesheet" href="{{ asset('css/jquery.timepicker.css') }}">
-<link rel="stylesheet" href="{{ asset('css/quill.snow.css') }}">>
+{{-- <link rel="stylesheet" href="{{ asset('css/feather.css') }}">
+<link rel="stylesheet" href="{{ asset('css/select2.css') }}"> --}}
+{{-- <link rel="stylesheet" href="{{ asset('css/dropzone.css') }}">
+<link rel="stylesheet" href="{{ asset('css/uppy.min.css') }}"> --}}
+{{-- <link rel="stylesheet" href="{{ asset('css/jquery.steps.css') }}">
+<link rel="stylesheet" href="{{ asset('css/jquery.timepicker.css') }}"> --}}
+{{-- <link rel="stylesheet" href="{{ asset('css/quill.snow.css') }}">> --}}
 
 <!-- Date Range Picker CSS -->
 <link rel="stylesheet" href="css/daterangepicker.css" />
