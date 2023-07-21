9c8963488ebb8290377d597e714802417df1c960
Andi Gigatera Halil M <54834402+agthm3@users.noreply.github.com>
2023-07-21 12:06:19 +0800

    changing all text to longtext on personal access token

--- database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
+++ database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
@@ -14,9 +14,9 @@ return new class extends Migration
         Schema::create('personal_access_tokens', function (Blueprint $table) {
             $table->id();
             $table->morphs('tokenable');
-            $table->string('name');
-            $table->string('token', 64)->unique();
-            $table->text('abilities')->nullable();
+            $table->longText('name');
+            $table->longText('token', 64)->unique();
+            $table->longText('abilities')->nullable();
             $table->timestamp('last_used_at')->nullable();
             $table->timestamp('expires_at')->nullable();
             $table->timestamps();
