3eceaa845f051ceeb90aa4475d10e58f9c2d9d74
Andi Gigatera Halil M <54834402+agthm3@users.noreply.github.com>
2023-08-02 12:20:22 +0800

    trying disable most of js

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -79,32 +79,32 @@
     <script src="{{ asset('dashboard/js/bootstrap.min.js') }}"></script>
     <script src="{{ asset('dashboard/js/simplebar.min.js') }}"></script>
     {{-- <script src="{{ asset('dashboard/js/daterangepicker.js') }}"></script> --}}
-    <script src="{{ asset('dashboard/js/jquery.stickOnScroll.js') }}"></script>
+    {{-- <script src="{{ asset('dashboard/js/jquery.stickOnScroll.js') }}"></script>
     <script src="{{ asset('dashboard/js/tinycolor-min.js') }}"></script>
     <script src="{{ asset('dashboard/js/config.js') }}"></script>
-    <script src="{{ asset('dashboard/js/d3.min.js') }}"></script>
-    <script src="{{ asset('dashboard/js/topojson.min.js') }}"></script>
+    <script src="{{ asset('dashboard/js/d3.min.js') }}"></script> --}}
+    {{-- <script src="{{ asset('dashboard/js/topojson.min.js') }}"></script>
     <script src="{{ asset('dashboard/js/datamaps.all.min.js') }}"></script>
     <script src="{{ asset('dashboard/js/datamaps-zoomto.js') }}"></script>
     <script src="{{ asset('dashboard/js/datamaps.custom.js') }}"></script>
-    <script src="{{ asset('dashboard/js/Chart.min.js') }}"></script>
+    <script src="{{ asset('dashboard/js/Chart.min.js') }}"></script> --}}
     <script>
         /* defind global options */
         Chart.defaults.global.defaultFontFamily = base.defaultFontFamily;
         Chart.defaults.global.defaultFontColor = colors.mutedColor;
     </script>
-    <script src="{{ asset('dashboard/js/gauge.min.js') }}"></script>
+    {{-- <script src="{{ asset('dashboard/js/gauge.min.js') }}"></script>
     <script src="{{ asset('dashboard/js/jquery.sparkline.min.js') }}"></script>
     <script src="{{ asset('dashboard/js/apexcharts.min.js') }}"></script>
     <script src="{{ asset('dashboard/js/apexcharts.custom.js') }}"></script>
     <script src="{{ asset('dashboard/js/jquery.mask.min.js') }}"></script>
-    <script src="{{ asset('dashboard/js/select2.min.js') }}"></script>
-    <script src="{{ asset('dashboard/js/jquery.steps.min.js') }}"></script>
+    <script src="{{ asset('dashboard/js/select2.min.js') }}"></script> --}}
+    {{-- <script src="{{ asset('dashboard/js/jquery.steps.min.js') }}"></script>
     <script src="{{ asset('dashboard/js/jquery.validate.min.js') }}"></script>
     <script src="{{ asset('dashboard/js/jquery.timepicker.js') }}"></script>
     <script src="{{ asset('dashboard/js/dropzone.min.js') }}"></script>
     <script src="{{ asset('dashboard/js/uppy.min.js') }}"></script>
-    <script src="{{ asset('dashboard/js/quill.min.js') }}"></script>
+    <script src="{{ asset('dashboard/js/quill.min.js') }}"></script> --}}
     {{-- <script>
         $(".select2").select2({
             theme: "bootstrap4",
