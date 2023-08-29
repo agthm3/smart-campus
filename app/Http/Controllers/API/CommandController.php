b7505e8e8f5c84887032901ef80bd72dc5fa7708
Andi Gigatera Halil M <54834402+agthm3@users.noreply.github.com>
2023-08-29 20:23:19 +0800

    limit 20 row only return in api/commands

--- app/Http/Controllers/API/CommandController.php
+++ app/Http/Controllers/API/CommandController.php
@@ -10,7 +10,7 @@ class CommandController extends Controller
 {
     public function index()
     {
-        $commands = Command::all();
+        $commands = Command::latest()->limit(20)->get();
         return response()->json($commands);
     }
 }
