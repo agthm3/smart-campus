ed16beecc5c706053f3f140d90eadc8806e50e18
Andi Gigatera Halil M <54834402+agthm3@users.noreply.github.com>
2023-09-18 20:03:40 +0800

    except /area-parkir/store for testing

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -14,6 +14,7 @@ class VerifyCsrfToken extends Middleware
     protected $except = [
         'open-barier/store',
         'api/handle-action',
-        'api/indoormonitoring'
+        'api/indoormonitoring',
+        '/area-parkir/store'
     ];
 }
